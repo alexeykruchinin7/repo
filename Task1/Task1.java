4e6d65641dc1316af72a71770f7f2dbaa5efe699
alexeykruchinin7 <126245391+alexeykruchinin7@users.noreply.github.com>
2023-03-30 21:16:09 +0300

    Update Task1.java

--- Task1/Task1.java
+++ Task1/Task1.java
@@ -1,19 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int now_pos = 0;
+        try
+        {
+            Scanner in = new Scanner(System.in);
+            String filename = in.next();
 
-        String result = "";
-        
-        while ((now_pos + m - 1) % n != 0) {
-            result += Integer.toString(now_pos + 1);
-            now_pos = (now_pos + m - 1) % n;
+            File file1 = new File(filename);
+            FileReader fr1 = new FileReader(file1);
+            BufferedReader reader1 = new BufferedReader(fr1);
+
+            String line1[] = reader1.readLine().split(" ");
+            int n = Integer.parseInt(line1[0]), m = Integer.parseInt(line1[1]);
+
+            int now_pos = 0;
+
+            String result = "";
+            
+            while ((now_pos + m - 1) % n != 0) {
+                result += Integer.toString(now_pos + 1);
+                now_pos = (now_pos + m - 1) % n;
+            }
+
+            System.out.println(result + Integer.toString(now_pos + 1));
         }
-
-        System.out.println(result + Integer.toString(now_pos + 1));
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
